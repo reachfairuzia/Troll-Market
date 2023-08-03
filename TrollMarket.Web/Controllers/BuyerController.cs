c51ea56dd4ba2ee25cd07990549c84fec7ee948a
Alfiaani <1736@LAPTOP-9OJ4JRNV>
2023-08-03 10:22:40 +0700

    topup dana

--- TrollMarket.Web/Controllers/BuyerController.cs
+++ TrollMarket.Web/Controllers/BuyerController.cs
@@ -1,14 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TrollMarket.Provider;
+using TrollMarket.ViewModel.Buyer;
 
 namespace TrollMarket.Web.Controllers
 {
     public class BuyerController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             var model = BuyerProvider.GetProvider().GetProfileTransaction();
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult TopUpDana(int BuyerId)
+        {
+            var model = new TopupDanaViewModel();
+            model.BuyerId = BuyerId;
+            model.Dana = 0;
+
+            return Json(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult TopUpDana([FromBody]TopupDanaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    BuyerProvider.GetProvider().TambahDana(model);
+                    return Json(new { success = true, message = "Berhasil Menyimpan", isvalid = true });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Gagal Menyimpan", isvalid = false });
+                }
+            }
+            catch
+            {
+                return Json(new { success = false, message = "Exception", isvalid = false });
+            }
         }
     }
 }
